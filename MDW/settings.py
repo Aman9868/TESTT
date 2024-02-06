d487113ed7d918107f9e3105c826cc7083fc8282
Aman9868 <9868638501aman@gmail.com>
2024-02-06 11:53:33 +0530

    dummy commit

--- MDW/settings.py
+++ MDW/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Note the trailing comma
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)  # Note the trailing comma
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
